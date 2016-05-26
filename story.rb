9d8675b9016d0285dfa325c41f1ca1ba3dd0981e
Masumi Nakamura <mnakamura1337@yahoo.co.jp>
2016-05-27 08:00:40 +0900

    Added story_merge

--- story.rb
+++ story.rb
@@ -1,4 +1,5 @@
 require 'json'
+require 'set'
 
 class Story
   VAR_HEADER = 'program = '
@@ -38,5 +39,38 @@ class Story
 
   def []=(k, v)
     @story[k] = v
+  end
+
+  class MergeConflict < Exception; end
+
+  def merge(other)
+    # meta
+    raise MergeConflict.new('meta mismatch') unless @story['meta'] == other['meta']
+
+    # imgs
+    merge_hashes(@story['imgs'], other['imgs'], 'imgs')
+
+    # chars
+    merge_hashes(@story['chars'], other['chars'], 'chars')
+
+    # script
+    @story['script'] += other['script']
+  end
+
+  private
+  def merge_hashes(h1, h2, hash_name)
+    h1_keys = Set.new(h1.keys)
+    h2_keys = Set.new(h2.keys)
+    h_common_keys = h1_keys.intersection(h2_keys)
+
+    h_common_keys.each { |k|
+      v1 = h1[k]
+      v2 = h2[k]
+      raise MergeConflict.new("#{hash_name} mismatch: key #{k}, got #{v1.inspect} vs #{v2.inspect}") unless v1 == v2
+    }
+
+    h2.each_pair { |k, v|
+      h1[k] = v
+    }
   end
 end
