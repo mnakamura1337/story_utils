67ac27e6ab11b5e75e7259a0bc3e758ada6ddff5
Masumi Nakamura <mnakamura1337@yahoo.co.jp>
2016-05-27 15:16:00 +0900

    Added story_join: allow joining of the same story in multiple languages

--- story.rb
+++ story.rb
@@ -57,6 +57,47 @@ class Story
     @story['script'] += other['script']
   end
 
+  def join(other)
+    # meta: compare sans orig_lang
+    m1 = @story['meta'].dup
+    m1.delete('orig_lang')
+    m2 = other['meta'].dup
+    m2.delete('orig_lang')
+    raise MergeConflict.new("meta mismatch: #{m1.inspect} vs #{m2.inspect}") unless m1 == m2
+
+    # imgs
+    merge_hashes(@story['imgs'], other['imgs'], 'imgs')
+
+    # TODO: check & join chars here
+
+    # script
+    @story['script'].size.times { |i|
+      c1 = @story['script'][i]
+      c2 = other['script'][i]
+
+      c1_op = c1['op']
+      c2_op = c2['op']
+
+      raise MergeConflict.new("script ##{i}: mismatch op: #{c1.inspect} vs #{c2.inspect}") unless c1_op == c2_op
+
+      case c1_op
+      when 'say', 'narrate', 'think', 'menu_add'
+        c1_char = c1['char']
+        c2_char = c2['char']
+        raise MergeConflict.new("script ##{i}: mismatch char: #{c1_char.inspect} vs #{c2_char.inspect}") unless c1_char == c2_char
+
+        c2['txt'].each_pair { |lang, txt|
+          if c1['txt'][lang] and c1['txt'][lang] != txt
+            raise MergeConflict.new("script ##{i}: mismatch txt for #{lang.inspect}: #{c1['txt'][lang]} vs #{txt}")
+          end
+          c1['txt'][lang] = txt
+        }
+      else
+        raise MergeConflict.new("script ##{i}: mismatch #{c1.inspect} vs #{c2.inspect}") unless c1 == c2
+      end
+    }
+  end
+
   private
   def merge_hashes(h1, h2, hash_name)
     h1_keys = Set.new(h1.keys)
